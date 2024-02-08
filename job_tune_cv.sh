9e943a42fc908f5880c8931c2ef8ef2e2540c8f2
Matt Hodgman <matthodg1@gmail.com>
2024-02-08 17:17:36 -0600

    Update job_tune_cv.sh

--- job_tune_cv.sh
+++ job_tune_cv.sh
@@ -4,14 +4,14 @@
 # directives that convey submission options:
 
 #SBATCH --job-name=tune_cv_hematoma_segmentation
-#SBATCH --mail-user=hodgman@umich.edu
+#SBATCH --mail-user=email@university.edu
 #SBATCH --mail-type=BEGIN,FAIL,END
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=24G
 #SBATCH --time=6:00:00
-#SBATCH --account=kayvan99
-#SBATCH -p gpu_mig40,gpu,spgpu
+#SBATCH --account=account_name
+#SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 
 module load python/3.10.4
