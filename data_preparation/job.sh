ded225e2afeea162d2262b2fb6be6b95bb7b00e6
Matt Hodgman <matthodg1@gmail.com>
2024-02-08 16:40:10 -0600

    Update job.sh

--- data_preparation/job.sh
+++ data_preparation/job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --mail-user=hodgman@umich.edu
+#SBATCH --mail-user=your_email@university.edu
 #SBATCH --mail-type=FAIL,END
 #SBATCH --job-name=job
 #SBATCH --nodes=1
@@ -7,8 +7,8 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=80g
 #SBATCH --time=6:00:00
-#SBATCH --account=kayvan99
-#SBATCH --partition=standard
+#SBATCH --account=slurm_account_name
+#SBATCH --partition=your_partition
 
 module load matlab/R2022b
 matlab -nodisplay -nodesktop -r 'runPreprocess'
